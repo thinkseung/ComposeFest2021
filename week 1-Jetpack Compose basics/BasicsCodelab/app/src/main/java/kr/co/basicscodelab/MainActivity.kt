673ede2c0cf1b8bf0609fd5226003d77dd418a58
장명승 <wkdaudtmd15@naver.com>
2021-11-15 18:44:33 +0900

    1주차 진행중

---
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/kr/co/basicscodelab/MainActivity.kt
@@ -0,0 +1,40 @@
+package kr.co.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContent 안에 XML 대신 레이아웃을 구성할 Composable 함수가 들어갈 수 있습니다.
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+// @Composable
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
+    }
+}
