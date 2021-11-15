434c42a0997a3ece5dfc9b7641592762a8babee5
장명승 <wkdaudtmd15@naver.com>
2021-11-15 19:02:26 +0900

    1주차 진행중

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/kr/co/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/kr/co/basicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package kr.co.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,13 +28,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// @Composable
+// @Composable 을 사용하면 Composable function으로 만들 수 있습니다.
+// Composable function 에는 TextView 대신 사용할 수 있는 Text 같은 Composable function 들을 넣을 수 있습니다.
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
-@Preview(showBackground = true)
+// @Preview를 통해 다양한 프리뷰를 볼 수 있습니다.
+@Preview(showBackground = true, name = "Text Preiview")
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
